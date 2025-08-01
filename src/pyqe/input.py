4b12feec3c1e2d2b1a5dd2b378a582df759751b7
Shibu Meher <56470750+Shibu778@users.noreply.github.com>
2025-08-01 21:48:06 +0530

    updated input

--- src/pyqe/input.py
+++ src/pyqe/input.py
@@ -7,12 +7,29 @@ class qe_input:
     Base class for Quantum ESPRESSO input files.
     """
 
-    def __init__(self, atoms, pseudopotentials, input_data):
-        self.atoms = atoms
-        self.pseudopotentials = pseudopotentials
+    def __init__(self, structure_file, input_data, pseudopotentials=None):
+        self.structure_file = structure_file
+        self.pseudopotentials = self.get_pseudopotentials(pseudopotentials)
         self.input_data = input_data
+        self.atoms = read(structure_file)
 
-    def write_input(self, filename):
+    def get_pseudopotentials(
+        self, pseudopotentials=None, default_pp="ONCV_PBE-1.2.upf"
+    ):
+        """
+        Get the pseudopotentials for the calculation.
+        """
+        if pseudopotentials is not None:
+            self.pseudopotentials = pseudopotentials
+            return self.pseudopotentials
+        else:
+            pseudopotentials = {}
+        for atom in set(self.atoms.get_chemical_symbols()):
+            pseudopotentials[atom] = atom + "_" + default_pp
+        self.pseudopotentials = pseudopotentials
+        return self.pseudopotentials
+
+    def write_input(self, filename="pw.in"):
         """
         Write the Quantum ESPRESSO input file.
         """
