ef9e2f5b06d2a36cd9c68775fee072314529f242
Shibu Meher <56470750+Shibu778@users.noreply.github.com>
2025-08-01 22:16:33 +0530

    Added example input set

--- src/pyqe/input.py
+++ src/pyqe/input.py
@@ -1,5 +1,6 @@
 # Python Classes to deal with input files for Quantum ESPRESSO
 from ase.io import write, read
+from pyqe.defaults import pseudo_directory, output_directory
 
 
 class qe_input:
@@ -9,9 +10,24 @@ class qe_input:
 
     def __init__(self, structure_file, input_data, pseudopotentials=None):
         self.structure_file = structure_file
-        self.pseudopotentials = self.get_pseudopotentials(pseudopotentials)
-        self.input_data = input_data
         self.atoms = read(structure_file)
+        self.pseudopotentials = self.get_pseudopotentials(pseudopotentials)
+        self.input_data = self.update_input_data(input_data)
+
+    def update_input_data(self, input_data):
+        """
+        Update the input data with new values.
+        """
+        if "output_dir" not in input_data:
+            input_data["outdir"] = output_directory
+        if "pseudo_dir" not in input_data:
+            input_data["pseudo_dir"] = pseudo_directory
+        if "nat" not in input_data:
+            input_data["nat"] = len(self.atoms)
+        if "ntyp" not in input_data:
+            input_data["ntyp"] = len(set(self.atoms.get_chemical_symbols()))
+
+        return input_data
 
     def get_pseudopotentials(
         self, pseudopotentials=None, default_pp="ONCV_PBE-1.2.upf"
---
+++ src/pyqe/input_sets.py
@@ -0,0 +1,14 @@
+# Set of inputs for work
+input1 = {
+    "calculation": "relax",
+    "restart_mode": "from_scratch",
+    "nstep": 200,
+    "prefix": "pbe",
+    "ecutwfc": 30,
+    "ecutrho": 300,
+    "etot_conv_thr": 1e-5,
+    "forc_conv_thr": 1e-2,
+    "conv_thr": 1e-8,
+    "ibrav": 0,
+    "vdw_corr": "dft-d3",
+}
